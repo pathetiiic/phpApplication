d1a2c55c15d4ffffeae98e6dc32a6b5adb4b9b3e
pathetic <94456023+pathetiiic@users.noreply.github.com>
2022-10-26 18:07:40 +0700

    add admin

--- views/pages/admin.php
+++ views/pages/admin.php
@@ -17,10 +17,23 @@
   Page::part('navbar');
 ?>
 
-<div class="container" style="display: flex; justify-content: space-between">
-  <div class="jumbotron mt-4">
-      <h1 class="display-4">Dashboard</h1>
-  </div>
+<div class="container">
+    <h2 class="mt-4">Добавить запись</h2>
+    <form action="/auth/admin" method="post" enctype="multipart/form-data" class="mt-4">
+        <div class="mb-3">
+            <label for="title" class="form-label">Title</label>
+            <input name="title" type="text" class="form-control" id="exampleInputEmail1">
+        </div>
+        <div class="mb-3">
+            <label for="about" class="form-label">Поробная информация</label>
+            <textarea name="about" class="form-control" id="about" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="articlePic" class="form-label">Изображение</label>
+            <input name="articlePic" type="file" class="form-control" id="avatar">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 </div>
 
 </body>
