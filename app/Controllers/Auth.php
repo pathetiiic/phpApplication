530da2db275defeb025588bb4a3d518015474fc1
pathetic <94456023+pathetiiic@users.noreply.github.com>
2022-10-26 18:09:30 +0700

    add method

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -66,6 +66,41 @@
         die();
       }
     }
+  
+    public function admin($data, $files): void
+    {
+      $title = $data['title'];
+      $about = $data['about'];
+      $articlePic = $files['articlePic'];
+      
+      $fileName = time() . '_' . $articlePic['name'];
+      $path = 'uploads/articles/' . $fileName;
+      
+      if (move_uploaded_file($articlePic['tmp_name'], $path)) {
+        $article = \R::dispense('articles');
+        
+        $article->title = $title;
+        $article->about = $about;
+        $article->articlePic = '/' . $path;
+        
+        \R::store($article);
+      } else {
+        Router::error('500');
+        die();
+      }
+      
+      $articles = \R::findAll('articles', 'ORDER BY id');
+      foreach ($articles as $item){
+        $_SESSION['articles'] = [
+          'id' => $item->id,
+          'title' => $item->title,
+          'about' => $item->about,
+          'articlePic' => $item->articlePic
+        ];
+        // todo: сделать вывод
+      }
+      Router::redirect('/articles');
+    }
     
     public function logout(): void
     {
